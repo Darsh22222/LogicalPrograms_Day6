d56e28985b47fb823cec40e3fe9d643084002b51
Darsh <darshankondhare2698@gmail.com>
2022-10-11 15:24:16 +0530

    StopWatch Program Added

---
+++ StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    class StopWatch
+    {
+        public static void Calculate()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(6);
+            }
+            sw.Stop();
+            Console.WriteLine("The elapse time is:{0:hh\\:mm\\:ss}", sw.Elapsed);
+        }
+    }
+}
